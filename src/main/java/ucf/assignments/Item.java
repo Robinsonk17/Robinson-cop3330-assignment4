dea75de8786d5935a4003d54006a55263e27d000
Robinsonk17 <84465115+Robinsonk17@users.noreply.github.com>
2021-07-06 00:15:27 -0400

    Create Item.java

---
+++ src/main/java/ucf/assignments/Item.java
@@ -0,0 +1,24 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 4 Solution
+ *  Copyright 2021 Keondez Robinson
+ */
+
+package ucf.assignments;
+
+public class Item {
+    // boolean field indicating current status
+    // String field indicating name
+    // String field as the formatted date
+
+    // Constructor ()
+    // -- initialize name and date passed in from user
+    // -- new item means status is incomplete, set to false
+
+    // Methods
+    // -- setter method for the status of item (setComplete())
+    // -- getter method for status (isComplete())
+    // -- setter method for the text of item (setText())
+    // -- getter method for text (getText())
+    // -- setter method for the date of item (setDate())
+    // -- getter method for date (getDate())
+}
