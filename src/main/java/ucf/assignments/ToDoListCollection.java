6606f18078b3e7f84dac52af17e7ab650f1c4222
Robinsonk17 <84465115+Robinsonk17@users.noreply.github.com>
2021-07-06 00:15:32 -0400

    Create ToDoListCollection.java

---
+++ src/main/java/ucf/assignments/ToDoListCollection.java
@@ -0,0 +1,35 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 4 Solution
+ *  Copyright 2021 Keondez Robinson
+ */
+
+package ucf.assignments;
+
+public class ToDoListCollection {
+    // private list of ToDoLists
+
+    // Constructor()
+    // -- can stay empty, this represents the start of the application. A user will be
+    //    presented with the option to load a list or create a new list
+    // -- for empty constructor, initialize the list present within the class
+
+    // Utility Methods
+    // addList(list)
+    // -- the method here assumes that a new ToDoList is created first
+    // -- in this case, grab the class's list of ToDoLists and call .add()
+
+    // removeList(list)
+    // -- wrapper for ToDoList.remove()
+
+    // saveList()
+    // -- the method will create a new file and write the contents of the list in an algorithm
+    //    known to the program
+    // -- format will save from the bottom-up, starting with each content of the item, then the list, then the
+    //    actual ToDoList
+
+    // loadList(file)
+    // -- using the format as specified in saveList(), the file passed in will be read and each
+    //    of the contents will be passed in to the objects as required
+    // -- once full ToDoList is created, pass inside to addList() to add the list to the running
+    //    list of ToDoLists
+}
