124d3947677cda71ee3e04c5fae86087e6fe9075
Robinsonk17 <84465115+Robinsonk17@users.noreply.github.com>
2021-07-25 17:34:19 -0400

    Create NeededTesting.java

---
+++ src/test/java/NeededTesting.java
@@ -0,0 +1,42 @@
+public class NeededTesting
+{
+    // addItem(item)
+    // -- Test if the items correctly adds
+
+
+    // removeItem(item)
+    // -- Test if items can properly be removed
+
+
+    // displayList(filter)
+    // -- Check that correct list displays when prompted
+
+
+    // addList(list)
+    // -- Check list is properly add
+
+    // removeList(list)
+    // -- Test if items can properly be removed
+
+    // saveList()
+    // -- Checks if lists are saved correctly
+
+    // loadList(file)
+    // -- Checks if list are loaded as instructed
+
+    // Constructor(name)
+
+     // (setName())
+    // -- Checks if named is set correctly
+    // (getName())
+    // (setItem(item, index))
+    // (getItem(index))
+
+
+
+
+
+
+
+
+}
